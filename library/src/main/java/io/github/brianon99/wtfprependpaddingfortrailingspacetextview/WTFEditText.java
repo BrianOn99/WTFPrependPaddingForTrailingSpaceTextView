250f38c7dbef04021fd8899b25fb85cd61377eed
BrianOn99 <chiubrian@link.cuhk.edu.hk>
2016-09-26 12:01:43 +0800

    WtFEditText subclass EditText

--- library/src/main/java/io/github/brianon99/wtfprependpaddingfortrailingspacetextview/WTFEditText.java
+++ library/src/main/java/io/github/brianon99/wtfprependpaddingfortrailingspacetextview/WTFEditText.java
@@ -1,81 +1,88 @@
 package io.github.brianon99.wtfprependpaddingfortrailingspacetextview;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.Selection;
 import android.text.TextUtils;
 import android.text.method.ArrowKeyMovementMethod;
 import android.text.method.MovementMethod;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
 /**
- * An almost copy-and-paste of android EditText source code
- * In ruby we can mixin, in java6 I can only copy and paste
- * Android source code is licensed in Apache 2.0 license.
- * http://www.apache.org/licenses/LICENSE-2.0
+ * An almost copy-and-paste of android TextView source code
  */
-public class WTFEditText extends WTFTextView {
+public class WTFEditText extends EditText {
     public WTFEditText(Context context) {
         super(context);
     }
 
     public WTFEditText(Context context, AttributeSet attrs) {
-        this(context, attrs, R.attr.editTextStyle);
+        super(context, attrs);
+       //this(context, attrs, R.attr.editTextStyle);
     }
 
     public WTFEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    /*
+     * Offset content from the left boundary, a hook method called in TextView#onDraw and
+     * onMeasure
+     */
     @Override
-    protected boolean getDefaultEditable() {
-        return true;
-    }
-
-    @Override
-    protected MovementMethod getDefaultMovementMethod() {
-        return ArrowKeyMovementMethod.getInstance();
-    }
-
-    @Override
-    public Editable getText() {
-        return (Editable) super.getText();
-    }
-
-    @Override
-    public void setText(CharSequence text, TextView.BufferType type) {
-        super.setText(text, TextView.BufferType.EDITABLE);
-    }
-
-    public void setSelection(int start, int stop) {
-        Selection.setSelection(getText(), start, stop);
-    }
-
-    public void setSelection(int index) {
-        Selection.setSelection(getText(), index);
-    }
-
-    public void selectAll() {
-        Selection.selectAll(getText());
-    }
-
-    public void extendSelection(int index) {
-        Selection.extendSelection(getText(), index);
-    }
-
-    @Override
-    public void setEllipsize(TextUtils.TruncateAt ellipsis) {
-        if (ellipsis == TextUtils.TruncateAt.MARQUEE) {
-            throw new IllegalArgumentException("EditText cannot use the ellipsize mode "
-                    + "TextUtils.TruncateAt.MARQUEE");
+    public int getCompoundPaddingLeft() {
+        int base_padding = super.getCompoundPaddingLeft();
+        if (!shouldPrependPadding()) {
+            return base_padding;
+        } else {
+            return base_padding + (int) (getTrailingSpaceWidth() / 2);
         }
-        super.setEllipsize(ellipsis);
     }
 
+    /*
+     * This could be negative, to make some trailing space not shown up
+     */
     @Override
-    public CharSequence getAccessibilityClassName() {
-        return EditText.class.getName();
+    public int getCompoundPaddingRight() {
+        int base_padding = super.getCompoundPaddingRight();
+        if (!shouldPrependPadding()) {
+            return base_padding;
+        } else {
+            return base_padding - (int) (getTrailingSpaceWidth() / 2);
+        }
+    }
+
+    private float getTrailingSpaceWidth() {
+        CharSequence text = getText();
+        int spaceCount = trailingSpaceCount(text);
+        /* TextView#onMeasure use this method to calculate width */
+        return Layout.getDesiredWidth(text, text.length() - spaceCount, text.length(), getPaint());
+    }
+
+    private static int trailingSpaceCount(CharSequence text) {
+        int len = text.length();
+        int i;
+        for (i=len-1; i>=0; i--) {
+            if (text.charAt(i) != ' ')
+                break;
+        }
+        return (len-1) - i;
+    }
+
+    private boolean shouldPrependPadding() {
+        if ((getGravity() & Gravity.HORIZONTAL_GRAVITY_MASK) != Gravity.CENTER_HORIZONTAL)
+            return false;
+
+        if (getLayoutParams().width != ViewGroup.LayoutParams.WRAP_CONTENT)
+            return false;
+
+        CharSequence text = getText();
+        return (text.length() == 0) || (text.charAt(text.length()-1) == ' ');
     }
 }
